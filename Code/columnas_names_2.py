bf4103e619d049d8442da69c2c550641644742c5
Jaime Rodriguez Lara <Jaime.Rodriguez.Lara@ibm.com>
2025-01-25 18:16:36 +0100

    prueba

--- Code/columnas_names_2.py
+++ Code/columnas_names_2.py
@@ -1,19 +1,3 @@
-'''import pandas as pd
-
-# Ruta del archivo CSV
-file_path = 'C:/Users/Administrador.HP_Jaime/Desktop/TFG-main/Data/PARAMETROS_TJ2.csv'
-
-# Leer las primeras 5 filas del archivo para inspeccionar las columnas
-
-df_preview = pd.read_csv(file_path, encoding='utf-8', nrows=5)
-
-#print(df_preview.columns)
-
-#df['fecha'] = pd.to_datetime(df['fecha'].astype(str), format='%Y%m%d')
-
-print(df_preview['hora'].head())  # Ver los primeros 5 valores de la columna 'fecha'
-print(df_preview['hora'].isnull().sum())  # Ver cuántos valores nulos hay en la columna 'fecha'''
-
 import pandas as pd
 
 # Ruta del archivo CSV
