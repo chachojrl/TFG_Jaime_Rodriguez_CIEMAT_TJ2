37a816d402a9bd71177f50bbf0f4ee8736a7e3a5
Jaime Rodriguez <jaime.rlara@alumnos.upm.es>
2025-01-27 20:06:40 +0100

    chat-with-ollama

--- Code/Prueba/pruebaOllama.py
+++
@@ -1,9 +0,0 @@
-import ollama
-
-prompt = "escribe un articulo sobre python" #input("Pregunta: ")
-
-modelo = "llama3.2"
-
-response = ollama.chat(model=modelo, messages = [{'role': 'user', 'content': prompt}])
-
-print(response)
---
+++ Code/chat_with_ollama.py
@@ -0,0 +1,35 @@
+import ollama
+import subprocess
+import re
+
+def chat_with_ollama():
+    print("Bienvenido al chat con Ollama. Escriba 'Salir' para finalizar la conversación")
+    
+    while True:
+        user_input = input("Tu: ")
+        
+        if user_input.lower() == 'salir':
+            print("Adios!!")
+            break
+        
+        # Verificar si el texto incluye "número de descarga" y señales específicas
+        if re.search(r'numero de descarga', user_input, re.IGNORECASE) and \
+           re.search(r'TFI|Densidad2_', user_input, re.IGNORECASE):
+            print("Ejecutando el script 'diagramasWeb.py'...")
+            
+            try:
+                # Ejecutar el fichero diagramasWeb.py
+                subprocess.run(["C:/Users/rodla/miniconda3/envs/tfg/python.exe", "./Code/diagramasWeb.py"], check=True)
+
+            except subprocess.CalledProcessError as e:
+                print("Hubo un error al ejecutar 'diagramasWeb.py':", e)
+            continue
+        
+        # Enviar el texto al modelo de Ollama
+        response = ollama.generate(model='llama3', prompt=user_input)
+        print("Bot: ", response['response'])
+
+# Iniciar el chat
+chat_with_ollama()
+
+# Me puedes dar la tabla del numero de descarga 57547 con TFI y Densidad2_
